392aec507557924ebe293249af8e7b5017eaf97d
Phmarcio <phablomrciomrcio54@gmail.com>
2024-07-22 21:49:35 -0300

    added loguru lib

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
-import logging
-from logging import info, debug, warning, error, critical
+from loguru import logger
+#from logging import info, debug, warning, error, critical
 
 app = Flask(__name__)
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+#logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
 
 @app.route('/')
@@ -12,27 +12,27 @@ def index():
 
 @app.route('/debug')
 def debug():
-    logging.debug('MENSAGEM DEBUG')
+    logger.debug('MENSAGEM DEBUG')
     return redirect(url_for("index"))
 
 @app.route('/info')
 def info():
-    logging.info('MENSAGEM INFO')
+    logger.info('MENSAGEM INFO')
     return redirect(url_for("index"))
 
 @app.route('/warn')
 def warn():
-    logging.warning('MENSAGEM WARNING')
+    logger.warning('MENSAGEM WARNING')
     return redirect(url_for("index"))
 
 @app.route('/error')
 def error():
-    logging.error('MENSAGEM ERROR')
+    logger.error('MENSAGEM ERROR')
     return redirect(url_for("index"))
 
 @app.route('/critical')
 def critical():
-    logging.critical('MENSAGEM CRITICAL')
+    logger.critical('MENSAGEM CRITICAL')
     return redirect(url_for("index"))
      
 
